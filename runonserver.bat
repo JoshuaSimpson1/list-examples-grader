9da72ad66d6b4cfc6dbd13991ad1d448c7bff12b
JoshuaSimpson1 <114563088+JoshuaSimpson1@users.noreply.github.com>
2022-11-10 11:46:08 -0800

    Add files via upload

---
+++ runonserver.bat
@@ -0,0 +1,2 @@
+scp -r list-examples-grader cs15lfa22kk@ieng6.ucsd.edu:~
+ssh cs15lfa22kk@ieng6.ucsd.edu "cd list-examples-grader; bash grade.sh https://github.com/ucsd-cse15l-f22/list-methods-corrected"
