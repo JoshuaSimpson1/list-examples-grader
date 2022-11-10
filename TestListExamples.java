9d9b59cd72c778e5fc3ef0311e1e0d9778bff3f8
JoshuaSimpson1 <114563088+JoshuaSimpson1@users.noreply.github.com>
2022-11-10 11:45:49 -0800

    Add files via upload

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,58 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*; 
+
+class StringCheckerTests implements StringChecker{
+    public boolean checkString(String s){
+        if(!s.equals("hello")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testFilter(){
+    StringChecker sc = new StringCheckerTests();
+
+    List<String> testAr = new ArrayList<>();
+    testAr.add("hello");
+    testAr.add("world");
+    testAr.add("I Love CSE 15L <3");
+
+    List<String> resultAr = ListExamples.filter(testAr,sc);
+    
+    List<String> expectedAr = new ArrayList<>();
+    expectedAr.add("world");
+    expectedAr.add("I Love CSE 15L <3");
+
+
+    assertEquals(resultAr, expectedAr);
+  }
+
+  @Test
+  public void testMerge(){
+    List<String> testAr = new ArrayList<>();
+    testAr.add("Apooky");
+    testAr.add("Bcary");
+
+    List<String> testAr2 = new ArrayList<>();
+    testAr2.add("Ckeletons");
+    testAr2.add("Dend Shivers Down");
+    testAr2.add("Eour spine");
+
+    List<String> resultAr = ListExamples.merge(testAr,testAr2);
+
+    List<String> expectedAr = new ArrayList<>();
+    expectedAr.add("Apooky");
+    expectedAr.add("Bcary");
+    expectedAr.add("Ckeletons");
+    expectedAr.add("Dend Shivers Down");
+    expectedAr.add("Eour spine");
+
+    assertEquals(expectedAr, resultAr);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,38 @@
+#!/bin/bash
 # Create your grading script here
-
-set -e
-
+#set -e
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
+
+cp TestListExamples.java student-submission/
+# In student submission
+cd student-submission
+
+LISTFILE=`find ListExamples.java`
+if [[ -e $LISTFILE ]]
+then
+    echo "List Examples Exists"
+else
+    echo "Uh oh you forgot to make the one file you needed"
+    exit 1
+fi
+
+javac -cp $CPATH *.java 
+if [[ $? -ne 0 ]]
+then
+    echo "Compile Unsuccessful Grade: F (For Feel Bad)"
+    exit 1
+else
+    echo "It compiled"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+if [[ $? -ne 0 ]]
+then
+    echo "You didn't pass, retake Math 4"
+else
+    echo "Probably not a failure"
+fi
 
